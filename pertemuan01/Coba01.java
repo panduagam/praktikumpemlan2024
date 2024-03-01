147ccbcb35dfd9144d3d3c871200b93be11fbdfe
panduagam <pandupanatagama123@gmail.com>
2024-03-01 07:22:00 +0700

    Ganti Sout

--- pertemuan01/Coba01.java
+++ pertemuan01/Coba01.java
@@ -2,6 +2,6 @@ package pertemuan01;
 
 public class Coba01 {
     public static void main(String[] args) {
-        System.out.println("Hallo Qimid");
+        System.out.println("Hallo Asha");
     }
 } 
